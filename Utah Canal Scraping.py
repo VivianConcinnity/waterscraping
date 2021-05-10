ff86071e1e72d3e421d1688f9b43695a765b7f77
VivianConcinnity <69015112+VivianConcinnity@users.noreply.github.com>
2021-05-10 12:43:28 -0500

    Update Utah Canal Scraping.py

--- Utah Canal Scraping.py
+++ Utah Canal Scraping.py
@@ -2,22 +2,29 @@
 """
 Created on Thu May  6 11:50:23 2021
 
-@author: dania
+@author: vivian
 """
 
+# Imports modules from selenium that are necessary to send inputs and read data.
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+
+# Imports the xlsxwriter module to write data to an Excel sheet.
 import xlsxwriter as xlsx
+
+# Imports the time module to implement waits for pages to load.
 import time
 
+# PATH stores the directory path to the Chrome webdriver file on the host's drive.
+# Download the webdriver at https://chromedriver.chromium.org/downloads
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
 def main():
     with webdriver.Chrome(PATH) as driver:
         try:
-            driver.get("https://waterrights.utah.gov/canalinfo/canal_owners.asp")
+            # Setup an Excel .xlsx file and format columns and rows.
             WORKBOOK = xlsx.Workbook("Utah Water Info.xlsx")
             WORKSHEET = WORKBOOK.add_worksheet()
             WORKSHEET.write(0, 0, "Id Number")
@@ -25,17 +32,25 @@ def main():
             WORKSHEET.write(0, 2, "County")
             WORKSHEET.write(0, 3, "Water Source")
             
+            # Open main page. . .
+            driver.get("https://waterrights.utah.gov/canalinfo/canal_owners.asp")
+            
+            # . . .and pull URLs from table that list Uintah as the county.
             urls = driver.find_elements_by_xpath("//tr[td[3]/font[contains(., 'Uintah')]]/td[1]/font/a")
 
             for index, box in enumerate(urls):
                 try:
+                    # For every URL from the table. . .
                     link = box.get_attribute("href")
                     
+                    # . . .opens and switches to a new window that navigates to the URL. . .
                     driver.execute_script("window.open(arguments[0]);", link)
                     driver.switch_to.window(driver.window_handles[1])
+                    
+                    # . . .wait half a second to let any time-sensitive elements load. . .
                     time.sleep(0.5)
                             
-                    # Collect information
+                    # . . .and collect the required information from the page.
                     # Try/except blocks are used in case the information isn't present on the page.
                     try:
                         companyId = driver.find_element_by_xpath("//input[@id='oldCompanyNumberId']").get_attribute("value")
@@ -60,7 +75,8 @@ def main():
                         WORKSHEET.write(index + 1, 3, waterSource)
                     except:
                         pass
-                            
+                    
+                    # After collecting information, the driver closes the window and switches back to the main window.
                     driver.close()
                     driver.switch_to.window(driver.window_handles[0])
                         
@@ -68,6 +84,7 @@ def main():
                     pass
 
         finally:
+            # After everything is done, close and save the .xlsx file and close the browser.
             WORKBOOK.close()
             driver.quit()
             
